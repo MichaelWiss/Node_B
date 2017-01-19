47e382a86c910f14c3402011782d8278d9ace22c
MichaelWiss <michael.wiss@gmail.com>
2017-01-19 16:00:38 -0600

    async setTimeout

---
+++ playground/async-basics.js
@@ -0,0 +1,15 @@
+console.log('Starting app');
+
+setTimeout(() => {
+   console.log('Inside of callback');
+}, 2000);
+
+setTimeout(() => {
+	console.log('Inside of timeout 2');
+}, 0)
+
+console.log('Finishing up');
+
+
+
+
