5a820d48f8e80f99571a27c70a3db699140cb767
MichaelWiss <michael.wiss@gmail.com>
2017-01-20 11:56:08 -0600

    formatted address

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
 	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
 	json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
 });
