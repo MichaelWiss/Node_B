df9047ed857e0e04c21c460ff300878960dbcf16
MichaelWiss <michael.wiss@gmail.com>
2017-01-20 14:47:11 -0600

    issue with accent grave

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const yargs = require('yargs');
 
 const argv = yargs
   .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+   a: {
+   	 demand: true,
+   	 alias: 'address',
+   	 describe: 'Address to fetch weather for',
+   	 string: true
+   }
+})
+.help()
+.alias('help', 'h')
+.argv;
 
 var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+	json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
