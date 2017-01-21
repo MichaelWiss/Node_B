433e1b4ea3543bc8874cb25ad2e2e5e14c0de3bb
MichaelWiss <michael.wiss@gmail.com>
2017-01-21 17:49:15 -0600

    darksky api call

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -13,11 +13,10 @@ var geocodeAddress = (address, callback) => {
 	       callback('Unable to find that address.');
 		} else if (body.status === 'OK') {
 			callback(undefined, {
-				address: body.results[0].formatted_address
-			})
-	    console.log(`Address: ${body.results[0].formatted_address}`);
-	    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-	    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+				address: body.results[0].formatted_address,
+				latitude: body.results[0].geometry.location.lat,
+				longitude: body.results[0].geometry.location.lng
+			});
 	   }
     });
 };
