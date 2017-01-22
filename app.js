970038dc8b69cab2f54a1ec39020eda7ebbf2793
MichaelWiss <michael.wiss@gmail.com>
2017-01-21 18:08:57 -0600

    error handling

--- app.js
+++ app.js
@@ -29,7 +29,11 @@ request({
 	url: 'https://api.darksky.net/forecast/51e75aca7a008ec7c34bb9530ec53ae8/45.0911599,-93.01124569999999',
 	json: true
 }, (error, response, body) =>{
-    console.log(body.currently.temperature);
+	if (!error && response.statusCode === 200) {
+		console.log(body.currently.temperature);
+	} else {
+		console.log('Unable to fetch weather.');
+	}
 });
 
 
