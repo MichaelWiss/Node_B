39720adf99bd215c2af601428726e478f1dcd747
MichaelWiss <michael.wiss@gmail.com>
2017-01-27 18:46:15 -0600

    weather request with darksky api

--- app.promise.js
+++ app.promise.js
@@ -19,7 +19,20 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
 
 axios.get(geocodeUrl).then((response) => {
+   if (response.data.status === 'ZERO_RESULTS') {
+       throw new Error('Unable to find that address.');
+   }
 
+   var lat = response.data.results[0].geometry.location.lat;
+   var long = response.data.results[0].geometry.locatio.long;
+   var weatherUrl = `https://api.darksky.net/forecast/51e75aca7a008ec7c34bb9530ec53ae8/${lat},${lng}`;
+   console.log(response.data);
+}).catch((e) => {
+  if (e.code === 'ENOTFOUND') {
+    console.log('Unable to connect to API servers.');
+  } else {
+    console.log(e.message);
+  }
 });
 
 
